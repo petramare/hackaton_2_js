f012b9207326f85d95cbeceebb561fc1f8fa75a8
OndrejVojik <vojik.o@hotmail.com>
2023-02-03 13:12:49 +0100

    part 2

---
+++ src/js/components/Event.js
@@ -0,0 +1,11 @@
+export class Event {
+    constructor(event){
+this.id=event.id;
+this.name=event.name;
+this.data=event.data;
+this.description=event.description;
+this.img_url=event.img_url;
+    }
+
+
+}
