52b13cad6f5bf023afcc3081687562c5c229ea66
OndrejVojik <vojik.o@hotmail.com>
2023-02-03 16:28:56 +0100

    finito

--- src/js/components/Event.js
+++ src/js/components/Event.js
@@ -1,13 +1,54 @@
 export class Event {
-    constructor(event){
-this.id=event.id;
-this.name=event.name;
-this.data=event.data;
-this.description=event.description;
-this.img_url=event.img_url;
-    }
+  constructor(event) {
+    this.event = event;
+    // this.id = event.id;
+    // this.name = event.name;
+    // this.data = event.data;
+    // this.description = event.description;
+    // this.img_url = event.img_url;
+    this.showModal = false;
+    this.element = document.createElement("div");
 
+    this.refreshHtml();
+  }
 
+  refreshHtml = () => {
+    // const div = document.createElement("div");
+    this.element.classList.add("events__card");
+    this.element.innerHTML = `
+        <h2 class="events__card-heading">${this.event.name}</h2>
+        <button class="events__card-btn">More</button>
+    <div class="modal${
+      this.showModal ? " modal_shown" : ""
+    }" style="background-image:url('${this.event.image_url}')"></div>
+        `;
+    // eventsList.appendChild(div);
+    const btn = this.element.querySelector("button.events__card-btn");
+    btn.addEventListener("click", () => {
+      this.showModal = !this.showModal;
+      if (this.showModal) {
+        const form = document.querySelector(".form");
+        form.classList.add("form_shown");
+      }
+      this.refreshHtml();
+    });
+    const registerBtn = document.querySelector(".full");
+    registerBtn.addEventListener("click", async (event) => {
+      event.preventDefault();
+      const url = `https://test-api.codingbootcamp.cz/api/77f3b34b/events/${this.event.id}/registrations`;
+      const myDataObject = {
+        email: document.querySelector("input[name=email]").value,
+        first_name: document.querySelector("input[name=name]").value,
+      };
+      const myResponse = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(myDataObject),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const myUsableResponse = await myResponse.json();
+      console.log(myUsableResponse);
+    });
+  };
 }
-
-
--- src/js/script.js
+++ src/js/script.js
@@ -12,38 +12,29 @@ import { Event } from "./components/Event";
 const loadData = async () => {
   const response = await fetch(
     "https://test-api.codingbootcamp.cz/api/c67330b3/events"
-    );
-    const data = await response.json();
-    console.log("request back with data", data);
-    
-    //list for navigation
-    const eventsList = document.querySelector(".events__list");
-    
-    //button for navigation    
-    data.forEach((object, index) => {
-      const div = document.createElement("div");
-        div.classList.add("events__card");
-      div.innerHTML = `
-        <h2 class="events__card-heading">${object.name}</h2>
-        <button id ="${object.id}"  class="events__card-btn">More</button>`;
-      eventsList.appendChild(div);
-   
-      
-    });
-    
-    //button click => showing articles
-    const btns = document.querySelectorAll(".events__card-btn");
-    
-    btns.forEach((eachBtn) => {
-      eachBtn.addEventListener(
-        "click", (event) => {
-          loadArticles(eachBtn.id);
-        }
-        );
-      });
-    };
+  );
+  const data = await response.json();
+  console.log("request back with data", data);
 
+  //list for navigation
+  const eventsList = document.querySelector(".events__list");
 
+  //button for navigation
+  data.forEach((object, index) => {
+    const event = new Event(object);
+    console.log(event);
+    eventsList.append(event.element);
+    //   const div = document.createElement("div");
+    //     div.classList.add("events__card");
+    //   div.innerHTML = `
+    //     <h2 class="events__card-heading">${object.name}</h2>
+    //     <button id ="${object.id}"  class="events__card-btn">More</button>`;
+    //   eventsList.appendChild(div);
+
+    //   const btn = document.getElementById(object.id)
+    //   btn.addEventListener("click",modalize)
+  });
+};
 
 // dynamicky slozena url
 const loadArticles = async (description) => {
@@ -51,7 +42,7 @@ const loadArticles = async (description) => {
     `https://test-api.codingbootcamp.cz/api/c67330b3/events=${description}`
   );
   const data = await response.json();
-    console.log(description);
+  console.log(description);
   console.log(data.data);
   const articleField = document.querySelector(".article");
   const eachArticle = document.createElement("div");
